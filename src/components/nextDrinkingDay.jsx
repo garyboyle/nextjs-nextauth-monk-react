7d48d1e1235fed2efe0c59ef15721655dc68b0cd
Gary Boyle <gary@shayoo.co.uk>
2020-10-19 20:53:45 +0100

    Tweak the layout

--- src/components/nextDrinkingDay.jsx
+++ src/components/nextDrinkingDay.jsx
@@ -7,48 +7,52 @@ export default function NextDrinkingDay({
   onMaxDrinkingDaysChange,
   onDrinkingDaysSoFarChange,
 }) {
-  const nextDrinkDay = Math.ceil((365 * drinkingDaysSoFar) / maxDrinkingDay);
+  const nextDrinkDay = Math.ceil(
+    (365 * (drinkingDaysSoFar + 1)) / maxDrinkingDay
+  );
   const nextDate = addDays(new Date(2020, 0, 1), nextDrinkDay);
 
   return (
-    <div className="jumbotron">
-      <h1 className="text-center">
+    <div className="vh-100 d-flex flex-column bg-light text-dark">
+      <h1 className="mb-auto text-center">
         <span className="small">Your next drinking day is </span> <br />
         {format(nextDate, "do MMM yyyy")}
       </h1>
 
-      <p>
-        <button
-          className="btn btn-primary m-2"
-          onClick={() => onMaxDrinkingDaysChange(1)}
-        >
-          +
-        </button>
-        <button
-          className="btn btn-primary m-2"
-          disabled={drinkingDaysSoFar >= maxDrinkingDay}
-          onClick={() => onMaxDrinkingDaysChange(-1)}
-        >
-          -
-        </button>
-        {maxDrinkingDay} max drinking days per year
-      </p>
-      <p>
-        <button
-          className="btn btn-primary m-2"
-          disabled={drinkingDaysSoFar >= maxDrinkingDay}
-          onClick={() => onDrinkingDaysSoFarChange(1)}
-        >
-          +
-        </button>
-        <button
-          className="btn btn-primary m-2"
-          onClick={() => onDrinkingDaysSoFarChange(-1)}
-        >
-          -
-        </button>
-        {drinkingDaysSoFar} days drinking so far
-      </p>
+      <div>
+        <p>
+          <button
+            className="btn btn-primary m-2"
+            onClick={() => onMaxDrinkingDaysChange(1)}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-primary m-2"
+            disabled={drinkingDaysSoFar >= maxDrinkingDay}
+            onClick={() => onMaxDrinkingDaysChange(-1)}
+          >
+            -
+          </button>
+          {maxDrinkingDay} max drinking days per year
+        </p>
+        <p>
+          <button
+            className="btn btn-primary m-2"
+            disabled={drinkingDaysSoFar >= maxDrinkingDay}
+            onClick={() => onDrinkingDaysSoFarChange(1)}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-primary m-2"
+            onClick={() => onDrinkingDaysSoFarChange(-1)}
+          >
+            -
+          </button>
+          {drinkingDaysSoFar} days drinking so far
+        </p>
+      </div>
     </div>
   );
 }
