2702728a8107e38a6f785d0ddd568dec4d7d1827
Gary Boyle <gary@shayoo.co.uk>
2020-11-13 16:12:03 +0000

    Use next link

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { signOut } from "next-auth/client";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        Goals
-      </a>
+      <Link href="/">
+        <a className="navbar-brand" href="/">
+          Goals
+        </a>
+      </Link>
 
       <div>
-        <a className="btn btn-outline-dark m-2" href="/editUser" role="button">
-          My account
-        </a>
+        <Link href="/editUser">
+          <a className="btn btn-outline-dark m-2" role="button">
+            My account
+          </a>
+        </Link>
         <button className="btn btn-primary" onClick={signOut}>
           Sign out
         </button>
